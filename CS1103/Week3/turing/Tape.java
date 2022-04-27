b62ee6cb44ae9f9c229ef9cc367f1350373f3a62
unknown <01043028@ancor2.r.recruit.co.jp>
2022-04-27 16:30:11 +0900

    Modified Week3 source, but TestTuringMachine was not passed.

--- CS1103/Week3/turing/Tape.java
+++ CS1103/Week3/turing/Tape.java
@@ -1,24 +1,42 @@
 package turing;
+
+/**
+ * Tape class is used for a emulated memory device for Turing machine.
+ * This class contains several methods to manipulate the data on the tape and
+ * also move pointer to which cell is selected.
+ * This program is coded based on the concept of dual linked list. So each of
+ * cell has two links, prev and next, to two cells, which enable go back and
+ * forth easily.
+ *
+ * @author anonymous
+ * @version 1.0
+ *
+ */
 public class Tape {
 	private Cell currentCell;
-	public Tape(){
-			Cell new_Cell = new Cell();
-			new_Cell.content = ' ';
-			new_Cell.prev = null;
-			new_Cell.next = null;
-			currentCell = new_Cell;
-			
+
+	public Tape() {
+		Cell new_Cell = new Cell();
+		new_Cell.content = ' ';
+		new_Cell.prev = null;
+		new_Cell.next = null;
+		currentCell = new_Cell;
+
 	}
-	//Define each method needed for tape.
+
+	// Define each method needed for tape.
 	public Cell getCurrentCell() {
 		return currentCell;
 	}
+
 	public char getContent() {
 		return currentCell.content;
 	}
+
 	public void setContent(char ch) {
 		currentCell.content = ch;
 	}
+
 	public void moveLeft() {
 		if (currentCell.prev == null) {
 			Cell new_Cell = new Cell();
@@ -26,10 +44,11 @@ public class Tape {
 			new_Cell.next = currentCell;
 			currentCell.prev = new_Cell;
 			currentCell = new_Cell;
-		}else {
+		} else {
 			currentCell = currentCell.prev;
 		}
 	}
+
 	public void moveRight() {
 		if (currentCell.next == null) {
 			Cell new_Cell = new Cell();
@@ -37,10 +56,11 @@ public class Tape {
 			new_Cell.prev = currentCell;
 			currentCell.next = new_Cell;
 			currentCell = new_Cell;
-		}else {
+		} else {
 			currentCell = currentCell.next;
 		}
 	}
+
 	public String getTapeContents() {
 		String result = "";
 		Cell head = currentCell;
